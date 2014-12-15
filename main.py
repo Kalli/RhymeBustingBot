c03fa149c8960752ac67b0c4a7803d759e68813f
Karl Tryggvason <ktryggvason@gmail.com>
2014-12-15 22:29:45 +0000

    Changing tweet setup, minor refactoring

--- main.py
+++ main.py
@@ -15,31 +15,43 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 def rappers_step_up_to_me():
+    success = False
     rapper1 = echonest.get_familiar_rapper()
     rapper2 = echonest.get_familiar_rapper()
-    print rapper1["name"] + " vs. "+ rapper2["name"]
     print ""
+    print rapper1["name"] + " vs. "+ rapper2["name"]
     rapper1 = musixmatch.get_random_rap_lyrics(rapper1)
     rapper2 = musixmatch.get_random_rap_lyrics(rapper2)
-    l1 = rapper1["lyrics"].split("\n")
-    l2 = rapper2["lyrics"].split("\n")
-    h = rhyming.find_rhymes(l1, l2)
-    if h[0] > 0:
-        tweettext = rapper1["name"] + ": " + l1[h[1]]
-        tweettext += "\n"+rapper1["track_share_url"]
-        api.update_status(tweettext)
-        print tweettext
-        tweettext = rapper2["name"] + ": " + l2[h[2]]
-        tweettext += "\n"+rapper2["track_share_url"]
-        api.update_status(tweettext)
-        print tweettext
-        print ""
+    if "lyrics" in rapper1 and "lyrics" in rapper2:
+        rapper1["lyrics"] = rapper1["lyrics"].split("\n")
+        rapper2["lyrics"] = rapper2["lyrics"].split("\n")
+        rhyme = rhyming.find_rhymes(rapper1["lyrics"], rapper2["lyrics"])
+        if rhyme[0] > 0:
+            tweet_lyrics(rapper1, rapper2, rhyme)
+            success = True
+        else: 
+            print "No rhymes found"
     else: 
-        print "No rhymes found "
+        print "No lyrics found"
+    return success
+
+def tweet_lyrics(rapper1, rapper2, rhyme):
+    tweet1 = rapper1["name"] + ": " + rapper1["lyrics"][rhyme[1]]
+    tweet2 = rapper2["name"] + ": " + rapper2["lyrics"][rhyme[2]]
+    print tweet1
+    print tweet2
+    tweet1 += "\n" +rapper1["track_share_url"] 
+    tweet2 += "\n" +rapper2["track_share_url"] 
+    tweetid = api.update_status(tweet1).id_str
+    api.update_status(tweet2, tweetid).id_str
 
 while True:
     try:
-        rappers_step_up_to_me()
+        success = rappers_step_up_to_me()
+        time.sleep(20) 
+        if success:
+            time.sleep(60*60) 
+        else:
+            time.sleep(60)
     except Exception:
         print(traceback.format_exc())
-    time.sleep(60 * 60) # I never sleep cause sleep is the cousin of death?
